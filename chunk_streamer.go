75a652881771f6c785a1ac8b4fc3ac38cf556d62
Benjamin Pracht <benjamin@livekit.io>
2025-10-31 16:47:30 -0700

    Read extended timestamps from fmt=1,2,3 chunks when present (#6)
    This fixes a bug in the go-rtmp chunk parsing.

--- chunk_streamer.go
+++ chunk_streamer.go
@@ -200,12 +200,6 @@ func (cs *ChunkStreamer) readChunk() (*ChunkStreamReader, error) {
 	}
 	//cs.logger.Debugf("(READ) BasicHeader = %+v", bh)
 
-	var mh chunkMessageHeader
-	if err := decodeChunkMessageHeader(cs.r, bh.fmt, cs.cacheBuffer, &mh); err != nil {
-		return nil, err
-	}
-	//cs.logger.Debugf("(READ) MessageHeader = %+v", mh)
-
 	reader, err := cs.prepareChunkReader(bh.chunkStreamID)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Failed to prepare chunk reader")
@@ -214,6 +208,12 @@ func (cs *ChunkStreamer) readChunk() (*ChunkStreamReader, error) {
 		reader.buf.Reset()
 		reader.completed = false
 	}
+
+	var mh chunkMessageHeader
+	if err := decodeChunkMessageHeader(cs.r, bh.fmt, cs.cacheBuffer, &mh, reader.messageHeader); err != nil {
+		return nil, err
+	}
+	//cs.logger.Debugf("(READ) MessageHeader = %+v", mh)
 
 	reader.basicHeader = bh
 	reader.messageHeader = mh
